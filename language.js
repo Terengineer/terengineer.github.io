a9f1b5751e561622e9bd4768253612affdac9d13
Terengineer <160143215+Terengineer@users.noreply.github.com>
2026-09-11 23:16:57 +0800

    Missing some added, and fixed text

--- language.js
+++ language.js
@@ -96,15 +96,16 @@ const translations = {
         ruleshead:"Rules (Official launched)",
         rulesb1:"Live Stream, Shorts, and Premiere Rules on YouTube in description",
         rulesb2:"Strictly prohibited🚫:",
-        rulesp1:"1. Without speak Traditional Chinese, and English (Except Earthquake monitoring live)!~",
+        rulesp1:"1. No speak Traditional Chinese, and English (Except Earthquake monitoring live)!~",
         rulesp2:"2. No Serious call my Nickname!~",
         rulesp3:"3. Hack, Attack, and Spam (Include too much/many)!~",
-        rulesp4:"4. Report all (Except some viewer violation rule(s), Malicious will be permanent block)!~",
+        rulesp4:"4. Report all (Except some viewer violated rule(s), Malicious will be permanent block)!~",
         rulesp5:"5. Call Emergency (Malicious will be permanent block)!~",
         rulesp6:"6. Announcement anything (Except Shout-out) 📢, put unknown link, and nsfw link (Include Scam)!~",
         rulesp7:"7. Press dislike button👎 (Only on YouTube)!~",
         rulesp8:"8. Any political!~",
-        rulesp9:"9. In-game False vote kick!~",
+        rulesp9:"9. Publish, reprint, quote or allude to any remarks or articles related to earthquake prediction on Earthquakes cannot be effectively predicted (To avoid violating the Meteorological Law or the Public Order Maintenance Law, Earthquake livestream only)!~",
+        rulesp10:"10. In-game False vote kick!~",
         rulesb3:"⚠️If you do it a violated rule(s), 1st Remove (Warn), 2nd Report (Mute), 3rd Permanent Block (Ban), and Report by Terengineer (Include joined Terengineer Discord)!~",
         rulesb4:"🤝Sponsored for business inquiries e-mail (Only Legitimate for 3C product, and Game, Strictly prohibited illegal, and others!~):",
         official:"On June, 16th, 2024, The Rules has been officially launched!",
@@ -129,7 +130,8 @@ const translations = {
         rulesp6:"6. 任何事物公告（除了 Shout-out 以外）📢，放不明連結和色情連結！~",
         rulesp7:"7. 按不喜歡👎（只有在YT）！~",
         rulesp8:"8. 任何政治！~",
-        rulesp9:"9. 遊戲內假票踢出！~",
+        rulesp9:"9. 地震無法有效預測，發布、轉載、引用或提及任何與地震預測相關的言論或文章（避免違反《氣像法》或《社會秩序維護法》，僅限地震直播）！~",
+        rulesp10:"10. 遊戲內假票踢出！~",
         rulesb3:"⚠️如果您違反規則，第一次移除（警告），第二次檢舉（靜音），第三次永久封鎖和檢舉由阿祐先生（包含已加入的阿祐先生DC）！~",
         rulesb4:"🤝贊助業務諮詢電子郵件（只有合法的3C產品和遊戲，嚴禁非法和其它！~）:",
         official:"2024年6月16日，本規則已正式啟動！",
@@ -297,6 +299,7 @@ let rulesp6 = document.getElementById("rulesp6");
 let rulesp7 = document.getElementById("rulesp7");
 let rulesp8 = document.getElementById("rulesp8");
 let rulesp9 = document.getElementById("rulesp9");
+let rulesp10 = document.getElementById("rulesp10");
 let rulesb3 = document.getElementById("rulesb3");
 let rulesb4 = document.getElementById("rulesb4");
 /*Equipment*/
@@ -450,6 +453,7 @@ const setLanugage = (language) => {
         rulesp7.innerText = translations.ruleszhcn.rulesp7;
         rulesp8.innerText = translations.ruleszhcn.rulesp8;
         rulesp9.innerText = translations.ruleszhcn.rulesp9;
+        rulesp10.innerText = translations.ruleszhcn.rulesp10;
         rulesb3.innerText = translations.ruleszhcn.rulesb3;
         rulesb4.innerText = translations.ruleszhcn.rulesb4;
         official.innerText = translations.ruleszhcn.official;
@@ -475,6 +479,7 @@ const setLanugage = (language) => {
         rulesp7.innerText = translations.rulesen.rulesp7;
         rulesp8.innerText = translations.rulesen.rulesp8;
         rulesp9.innerText = translations.rulesen.rulesp9;
+        rulesp10.innerText = translations.rulesen.rulesp10;
         rulesb3.innerText = translations.rulesen.rulesb3;
         rulesb4.innerText = translations.rulesen.rulesb4;
         official.innerText = translations.rulesen.official;
